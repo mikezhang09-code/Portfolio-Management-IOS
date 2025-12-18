9d20a65bfc3a8bdcf6600782d35afb9b73716855
mikezhang09-code <mike.zhang09@gmail.com>
2025-12-18 13:47:05 +0800

    Initial Commit

---
+++ Portfolio-Management-System/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Portfolio-Management-System
+//
+//  Created by admin on 2025/12/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Portfolio-Management-System/Portfolio_Management_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Portfolio_Management_SystemApp.swift
+//  Portfolio-Management-System
+//
+//  Created by admin on 2025/12/18.
+//
+
+import SwiftUI
+
+@main
+struct Portfolio_Management_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
